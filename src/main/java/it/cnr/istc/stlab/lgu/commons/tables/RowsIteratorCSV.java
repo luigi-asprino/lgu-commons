f819185c8fafbc8e72bf81868a55941f2e9b26dc
Luigi Asprino <luigi.asprino@gmail.com>
2019-06-19 10:04:37 +0200

    mvn repository

--- src/main/java/it/cnr/istc/stlab/lgu/commons/tables/RowsIteratorCSV.java
+++ src/main/java/it/cnr/istc/stlab/lgu/commons/tables/RowsIteratorCSV.java
@@ -9,23 +9,60 @@ import au.com.bytecode.opencsv.CSVReader;
 public class RowsIteratorCSV implements RowsIterator {
 
 	private CSVReader reader;
+	private String[] nextRow;
+	private boolean readNextCalled = false;
 
 	public RowsIteratorCSV(String file, char separator) throws FileNotFoundException {
 		super();
 		this.reader = new CSVReader(new FileReader(file), separator);
 	}
 
-	private String[] nextRow;
-
 	@Override
 	public String[] next() {
-		return nextRow;
+		if (nextRow == null) {
+			if (readNextCalled) {
+				return null;
+			} else {
+				String[] result;
+				try {
+					result = reader.readNext();
+					readNextCalled = true;
+					nextRow = reader.readNext();
+					return result;
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		} else {
+			try {
+				String[] result = nextRow;
+				nextRow = reader.readNext();
+				return result;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return null;
+
 	}
 
 	@Override
 	public boolean hasNext() {
 		try {
-			return (nextRow = reader.readNext()) != null;
+
+			if (nextRow == null && !readNextCalled) {
+				if (readNextCalled) {
+					return false;
+				} else {
+					nextRow = reader.readNext();
+					readNextCalled = true;
+					return nextRow != null;
+				}
+			} else {
+				return true;
+			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
