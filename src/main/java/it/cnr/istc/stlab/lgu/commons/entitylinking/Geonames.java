e1a3e74dc34b8d43da72d349256f748fa791b57c
Luigi Asprino <luigi.asprino@gmail.com>
2019-10-05 10:01:54 +0200

    Fix geonames

--- src/main/java/it/cnr/istc/stlab/lgu/commons/entitylinking/Geonames.java
+++ src/main/java/it/cnr/istc/stlab/lgu/commons/entitylinking/Geonames.java
@@ -7,6 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.ResIterator;
@@ -16,6 +20,7 @@ import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.vocabulary.RDF;
 
 import it.cnr.istc.stlab.lgu.commons.files.FileUtils;
+import it.cnr.istc.stlab.lgu.commons.model.Entity;
 import it.cnr.istc.stlab.lgu.commons.web.HTTPUtils;
 
 public class Geonames {
@@ -30,6 +35,11 @@ public class Geonames {
 
 		StringBuilder sb = new StringBuilder();
 		sb.append(URL_BASE);
+
+		if (USERNAME == null) {
+			USERNAME = FileUtils.readFile("geonames.username");
+		}
+
 		sb.append("&username=" + USERNAME);
 
 		if (country != null) {
@@ -86,22 +96,42 @@ public class Geonames {
 	public static List<String> getGeoNames(String text) throws IOException {
 		return getGeoNames(text, "IT", "it", new String[] { "A", "L" });
 	}
-	
+
 	public static String getGeoNamesRDF(String text) throws IOException {
 		return getGeoNamesRDF(text, "IT", "it", new String[] { "A", "L" });
+	}
+
+	public static String getGeoNamesRDF(String text, String[] featureClass) throws IOException {
+		return getGeoNamesRDF(text, "IT", "it", featureClass);
 	}
 
 	public static List<String> getGeoNames(String text, String[] featureClass) throws IOException {
 		return getGeoNames(text, "IT", "it", featureClass);
 	}
 
-	public static void main(String[] args) throws IOException {
-		getGeoNames("Europa").forEach(u -> {
-			System.out.println(u);
-		});
-		getGeoNames("Albano Laziale ").forEach(u -> {
-			System.out.println(u);
-		});
+	public static Entity getGeolocalization(String text, String[] featureClass) throws IOException {
+		String q2 = "PREFIX gn: <http://www.geonames.org/ontology#> "
+				+ "PREFIX wgs84: <http://www.w3.org/2003/01/geo/wgs84_pos#> " + "SELECT ?lat ?lon {"
+				+ "?f a gn:Feature . " + "?f wgs84:lat ?lat . " + "?f wgs84:long ?lon . " + "}";
+
+		Model gn_model = ModelFactory.createDefaultModel();
+		RDFDataMgr.read(gn_model, new ByteArrayInputStream(getGeoNamesRDF(text, featureClass).getBytes()), Lang.RDFXML);
+		QueryExecution qexecGn = QueryExecutionFactory.create(q2, gn_model);
+		ResultSet rsGn = qexecGn.execSelect();
+		if (rsGn.hasNext()) {
+			QuerySolution qsGn = (QuerySolution) rsGn.next();
+			String lat = qsGn.getLiteral("lat").toString();
+			String lon = qsGn.getLiteral("lon").toString();
+			Entity e = new Entity();
+			e.setLatitude(lat);
+			e.setLongitude(lon);
+			return e;
+		}
+		return null;
+	}
+
+	public static Entity getGeolocalization(String text) throws IOException {
+		return getGeolocalization(text, new String[0]);
 	}
 
 }
