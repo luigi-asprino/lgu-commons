39240e2393bf83ac4a58aed24ce4e1ee9b0f5147
Luigi Asprino <luigi.asprino@gmail.com>
2019-11-14 08:00:57 +0100

    RDF2Vec

---
+++ src/main/java/it/cnr/istc/stlab/lgu/commons/rdf/QueryExecutor.java
@@ -0,0 +1,29 @@
+package it.cnr.istc.stlab.lgu.commons.rdf;
+
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.tdb.TDBFactory;
+
+public class QueryExecutor {
+
+	public static void queryTDB(String tdbPath, String query) {
+		Dataset d = TDBFactory.createDataset(tdbPath);
+		Query q = QueryFactory.create(query);
+		d.begin(ReadWrite.READ);
+		QueryExecution qe = QueryExecutionFactory.create(q, d);
+		if (q.isSelectType()) {
+			System.out.println(ResultSetFormatter.asText(qe.execSelect()));
+		}
+		d.end();
+	}
+	
+	public static void main(String[] args) {
+		queryTDB("/Users/lgu/Desktop/fn17", "SELECT * {?s ?p ?o} LIMIT 10");
+	}
+
+}
---
+++ src/main/java/it/cnr/istc/stlab/lgu/commons/rdf/RDFTransformer.java
@@ -0,0 +1,61 @@
+package it.cnr.istc.stlab.lgu.commons.rdf;
+
+import java.io.File;
+
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.tdb.TDBFactory;
+
+public class RDFTransformer {
+
+	public enum Format {
+		TTL, TDB
+	}
+
+	private Format in, out;
+	private String pathFileIn, pathFileOut, tempFolder;
+
+	public RDFTransformer(Format in, Format out, String pathFileIn, String pathFileOut, String tempFolder) {
+		super();
+		this.in = in;
+		this.out = out;
+		this.pathFileIn = pathFileIn;
+		this.pathFileOut = pathFileOut;
+		this.tempFolder = tempFolder;
+	}
+
+	public void transform() {
+		new File(tempFolder).mkdir();
+
+		Dataset ds;
+		if (out == Format.TDB) {
+			ds = TDBFactory.createDataset(pathFileOut);
+		} else {
+			ds = TDBFactory.createDataset(tempFolder + "/tdb");
+		}
+
+		switch (in) {
+		case TTL:
+			Model m = ModelFactory.createDefaultModel();
+			RDFDataMgr.read(m, pathFileIn);
+			ds.begin(ReadWrite.WRITE);
+			ds.getDefaultModel().add(m);
+			ds.commit();
+			ds.close();
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		RDFTransformer r = new RDFTransformer(Format.TTL, Format.TDB, "/Users/lgu/Desktop/fn17.ttl", "/Users/lgu/Desktop/fn17",
+				"/Users/lgu/Desktop/tmp");
+		r.transform();
+	}
+
+}
